3238eae7e06b1e8e52d5861fc768135c0d19ea52
fixcik <chirkin.ivan@gmail.com>
2023-11-16 19:41:08 +0300

    improve

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Rust bindings for libchdb
 
 See: https://github.com/metrico/libchdb
+
+Required to install `libchdb`:
+
+On ubuntu:
+```bash
+wget -qO - https://metrico.github.io/metrico.gpg | apt-key add - 
+echo "deb [arch=x86_64] https://metrico.github.io/deb stable main" | tee /etc/apt/sources.list.d/metrico.list
+apt update && apt install -y libchdb
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+use std::{os::raw::{c_char, c_int}, ffi::c_void};
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct LocalResult {
+    pub buf: *const c_char,
+    pub size: usize,
+    _vec: *mut c_void,
+    pub elapsed: f64,
+    pub rows_read: u64,
+    pub bytes_read: u64,
+}
+
+#[link(name = "chdb")]
+extern "C" {
+    pub fn query_stable(argc: c_int, argv: *const *const c_char) -> *mut LocalResult;
+    pub fn free_result(result: *mut LocalResult);
+}
